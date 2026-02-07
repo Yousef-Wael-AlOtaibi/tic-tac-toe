36a6c5b6c87f26d6cdeca7d1fe17866059fca3f7
Yousef Wael Al Otaibi <ywalotaibi2010@gmail.com>
2026-02-07 08:17:40 +0300

    Create gameboardObject in a private IIFE module
    Create the gameboardObject and add the gameboard array to be able to
    later check game conditions.

---
+++ script.js
@@ -0,0 +1,13 @@
+const gameboardObject = (function() {
+    const gameboard = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+
+    
+
+    return {
+
+    }
+})();
