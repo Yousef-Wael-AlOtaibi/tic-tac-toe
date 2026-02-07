278fd2a129abf103530948c2ba7eaac269a629ca
Yousef Wael Al Otaibi <ywalotaibi2010@gmail.com>
2026-02-07 08:38:39 +0300

    Add gameboard getter and setter methods
    Since game board is a private variable, we will need to interact with it
    using getters and setters.

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ const gameboardObject = (function() {
         [null, null, null]
     ];
 
-    
+    const getGameboard = () => gameboard;
+
+    function markBoardCell(marker, rowIndex, columnIndex) {
+        gameboard[rowIndex][columnIndex] = marker;
+     };
 
     return {
-
-    }
+        getGameboard,
+        markBoardCell
+    };
 })();
+
+function createPlayer(name, marker) {
+
+    return {
+        name,
+        marker
+    }
+}
